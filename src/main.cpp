0c77f9c161412ff4eb60e1f8cb21d60a4c712d4d
Georgiy Kulak <kulakgeorgij@gmail.com>
2021-02-01 08:06:12 +0200

    Refactor code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,8 @@
 int main()
 {
     BoolRPE expression;
-    int bits, variables;
+    int bits;
+    int variables;
     char answer = 'y';
     bool flag = false;
     std::string expr_string;
@@ -12,56 +13,71 @@ int main()
     help();
 
     std::cout << "Enter your function\n";
-    getline(std::cin, expr_string);
+    getline( std::cin, expr_string );
 
-    while(answer == 'y' || answer == 'Y')
+    while ( answer == 'y' || answer == 'Y' )
     {
-        analysis(expr_string, &variables);
-        expression.setNumbOfVar(variables);
+        analysis( expr_string, &variables );
+        expression.setNumbOfVar( variables );
         std::cout << "Do you want to add elements manually? (y/n) ";
         std::cin >> answer;
-        if(answer != 'y' && answer != 'Y')
+        
+        if ( answer != 'y' && answer != 'Y' )
         {
             std::cout << "Select the option (r - random / t - truth table) ";
             std::cin >> answer;
-            if(answer == 'r' || answer == 'R')
+            
+            if ( answer == 'r' || answer == 'R' )
             {
                 std::cout << "Enter the digit depth of variables ";
                 std::cin >> bits;
-                expression.setBitSize(bits);
+                
+                expression.setBitSize( bits );
                 expression.randomSetX();
             }
             else
             {
                 expression.truth_table();
+                
                 std::cout << "Do you want to see classification of boolean function? (y/n) ";
                 std::cin >> answer;
-                if(answer == 'y' || answer == 'Y') flag = true;
+                
+                if ( answer == 'y' || answer == 'Y' )
+                    flag = true;
             }
         }
         else
         {
             std::cout << "Decimal or bitwise enter? (d/b) ";
             std::cin >> answer;
-            if(answer == 'b' || answer == 'B')
+            
+            if ( answer == 'b' || answer == 'B' )
             {
                 std::cout << "Enter the digit depth of variables ";
                 std::cin >> bits;
-                expression.setBitSize(bits);
+                
+                expression.setBitSize( bits );
                 expression.setX();
             }
-            else expression.setXdec();
+            else
+            {
+                expression.setXdec();
+            }
         }
+        
         expression.getX();
-        expression.setExample(expr_string);
+        expression.setExample( expr_string );
         expression.calculate_expression();
         expression.getAnswer();
-        if(flag) expression.classification();
+        
+        if ( flag )
+            expression.classification();
+        
         flag = false;
+        
         std::cout << "Do you want to continue? (y/n) ";
         std::cin >> answer;
     }
 
     return 0;
 }
-
